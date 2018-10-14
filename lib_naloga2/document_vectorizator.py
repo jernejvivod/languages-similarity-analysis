b01ea66b4a5ef759f0a2b72eebc41b34960c7aa0
Jernej Vivod <je.vivod@gmail.com>
2018-10-14 10:37:23 +0200

    Refactor source files, add parallelization for idf computation

---
+++ lib_naloga2/document_vectorizator.py
@@ -0,0 +1,59 @@
+import math
+from lib_naloga2 import triplet_extractor
+from joblib import Parallel, delayed
+import multiprocessing
+
+########################
+# Author: Jernej Vivod #
+########################
+
+# count_containing: count number of documents that contain specified triplet and return result.
+def count_containing(results_dict, triplet):
+	counter = 0 		# Initialize counter.
+	for doc in results_dict.keys(): 		# Go over documents.
+		if triplet in set(results_dict[doc].keys()): 	# If triplet contained in unique set of triplets found in document,
+			counter +=1 								# increment counter.
+	return counter
+
+
+# documents_to_vectors: take a list of document names and return dict that maps
+# document names to their attribute vectors computed using triplets found in documents and their
+# frequencies and inverse document frequencies
+#
+# The vectors representing each document are represented as dictionaries.
+def documents_to_vectors(documents):
+	results_dict = dict()					# Define dictionary for storing the results.
+	triplets_global = set() 				# Define matrix for storing unique triplets found in all documents.
+
+	# Go over documents in list of document names.
+	for document in documents:
+		document_text = open("./data/translations/" + document, encoding="utf8").read() 	# Parse document
+		# Get dictionary that maps unique triplets to their relative frequencies
+		triplets_dict = triplet_extractor.text_to_triplets_dict(document_text)
+		triplets_global |= set(list(triplets_dict.keys())) 			# Add found unique triplets to global set of unique triplets
+		results_dict[document] = triplets_dict
+
+
+	# Compute inverse document frequency for each triplet in global set of unique triplets.
+	idf_dict = multiprocessing.Manager().dict() 				# Define dictionary that maps each triplet to its idf.
+	num_documents = len(documents)
+
+	# compute_idfs: compute idf value for each triplet found in any of the documents and add
+	# entry to dictionary. This function is used in a parallelized loop implemented below.
+	def compute_idfs(triplet):
+		# Count number of documents in which the triplet appears.
+		num_containing = count_containing(results_dict, triplet)
+		# Compute inverse document frequency.
+		idf_dict[triplet] = math.log(num_documents / num_containing)
+
+	# Get number of cores on system.
+	num_cores = multiprocessing.cpu_count()
+	# Compute idf values in a parallelized loop.
+	Parallel(n_jobs=num_cores)(delayed(compute_idfs)(triplet) for triplet in triplets_global)
+
+	# Multiply relative frequencies in document vectors with the triplet's inverse document frequency.
+	for document in documents: 							# Go over documents.
+		for triplet in results_dict[document].keys(): 	# Go over triplets in next document.
+			results_dict[document][triplet] *= idf_dict[triplet] 	# Multiply relative frequency of triplet with its idf.
+	
+	return results_dict
---
+++ text_analyzer.py
@@ -0,0 +1,22 @@
+import os
+import numpy as np
+from lib_naloga2 import triplet_extractor
+from lib_naloga2 import document_vectorizator
+from lib_naloga2 import document_comparator
+
+# This script produces and saves a dictionary that maps each text file name to a dictionary that maps
+# each triplet that appears in the text file to its tf-idf value.
+
+########################
+# Author: Jernej Vivod #
+########################
+
+# Parse text files from folder.
+documents = set(os.listdir('./data/translations'))
+
+# Get dictionary where name of document file is mapped to dictionary that maps triplets
+# that appear in it to their tf-idf values.
+results_dict = document_vectorizator.documents_to_vectors(documents)
+
+# Save resulting dictionary to file.
+np.save('results_dict.npy', results_dict)
