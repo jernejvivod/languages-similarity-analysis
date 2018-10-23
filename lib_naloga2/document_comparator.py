924b01f84d62d714a56993290e43f90ab07d159f
Jernej Vivod <je.vivod@gmail.com>
2018-10-23 09:36:05 +0200

    Add functions that provide compatibility with hierarchical clustering

--- lib_naloga2/document_comparator.py
+++ lib_naloga2/document_comparator.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 ########################
 # Author: Jernej Vivod #
 ########################
@@ -45,3 +46,35 @@ def document_cosine_sim(doc_dict1, doc_dict2):
 	norm_1 = math.sqrt(sum(doc_dict1[key]**2 for key in doc_dict1.keys())) 				# Compute norms.
 	norm_2 = math.sqrt(sum(doc_dict2[key]**2 for key in doc_dict2.keys()))
 	return dot_product/(norm_1 * norm_2) # Compute and return cosine similarity.
+
+
+## Functions used with hierarchical clustering algorithm ###########################################################################
+
+# doc_dicts_to_vects: transform doc_dict1 and doc_dict2 to appropriate vectors.
+# See above notes for more information.
+# This function is used to transform document vectors to form that is compatible with the hierarchical clustering algorithm.
+def doc_dicts_to_vects(doc_dict1, doc_dict2):
+	# Define set of all triplets found in either document.
+	triplets_all = set(doc_dict1.keys()) | set(doc_dict2.keys())
+	# Allocate memory for vectors
+	vect1 = np.empty(len(triplets_all), dtype=float) 
+	vect2 = np.empty(len(triplets_all), dtype=float)
+ 	# Go over enumerated triplets.
+	for idx, triplet in enumerate(triplets_all):
+		if triplet in doc_dict1: 	# If triplet found in first document, add its tf-id value vector.
+			vect1[idx] = doc_dict1[triplet]
+		else:
+			vect1[idx] = 0 			# Else add zero entry.
+		if triplet in doc_dict2:
+			vect2[idx] = doc_dict2[triplet]
+		else:			
+			vect2[idx] = 0
+ 	return vect1, vect2 			# Return created vectors in order.
+
+# document_cosine_sim: compute cosine similarity between two documents using cosine similarity.
+# The function takes dictionaries that map triplets found in each document to its tf-idf value.
+def document_cosine_sim2(doc_dict1, doc_dict2):
+	vect1, vect2 = doc_dicts_to_vects(doc_dict1, doc_dict2)  					# Get vector representations of the two documents.
+	return np.dot(vect1, vect2)/(np.linalg.norm(vect1) * np.linalg.norm(vect2)) # Compute and return cosine similarity.
+
+######################################################################################################################################
